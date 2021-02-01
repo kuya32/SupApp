da6005fe12c5d7fecfa2a108fb176c3989f59dfb
Marchael Acode <m.acode@outlook.com>
2021-02-01 10:42:29 -0800

    Styling the side navigation drawer with header and other clickable items. Main activity app bar shows a clickable menu to access the side navigation drawer.

--- app/src/main/java/com/macode/supapp/MainActivity.java
+++ app/src/main/java/com/macode/supapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.macode.supapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.main_activity_appbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Sup App");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_hamburger);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        view = navigationView.inflateHeaderView(R.layout.drawer_header);
     }
 }
