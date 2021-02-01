a6320bb33b2f693913418074ac4a584e9ea24460
Marchael Acode <m.acode@outlook.com>
2021-02-01 11:38:42 -0800

    Depending on the user who logged in, the image and text oin the header of the side navigation will change accordingly.

--- app/src/main/java/com/macode/supapp/MainActivity.java
+++ app/src/main/java/com/macode/supapp/MainActivity.java
@@ -1,20 +1,42 @@
 package com.macode.supapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private View view;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference firebaseUserReference;
+    private String profileImageUrlData, usernameData;
+    private CircleImageView profileImageHeader;
+    private TextView usernameHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +49,78 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_hamburger);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        firebaseUserReference = FirebaseDatabase.getInstance().getReference().child("Users");
+
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navigationView);
 
         view = navigationView.inflateHeaderView(R.layout.drawer_header);
+        profileImageHeader = view.findViewById(R.id.profileImageDrawerHeader);
+        usernameHeader = view.findViewById(R.id.profileUsernameText);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (firebaseUser == null) {
+            sendToLogin();
+        } else {
+            firebaseUserReference.child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        profileImageUrlData = snapshot.child("profileImage").toString();
+                        usernameData = snapshot.child("username").toString();
+                        Picasso.get().load(profileImageUrlData).into(profileImageHeader);
+                        usernameHeader.setText(usernameData);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(MainActivity.this, "Sorry, something went wrong!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void sendToLogin() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.home) {
+
+        } else if (item.getItemId() == R.id.profile) {
+
+        } else if (item.getItemId() == R.id.friends) {
+
+        } else if (item.getItemId() == R.id.addFindFriends) {
+
+        } else if (item.getItemId() == R.id.chat) {
+
+        } else if (item.getItemId() == R.id.logout) {
+
+        } else {
+            Toast.makeText(this, "Impossibru! How did you do this!", Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return true;
     }
 }
